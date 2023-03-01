8f382a04fc2305f17212d88aaa4a864ab5990bf1
ThatOnePerson132 <118837311+ThatOnePerson132@users.noreply.github.com>
2023-03-01 09:38:09 -0800

    Chasing enemy
    The Enemy will chase the player and have a Sprite and prefab for enemy

---
+++ Assets/Kyle-Branch/Script/EnemyControler.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControler : MonoBehaviour
+{
+    public static event Action<EnemyControler> OnEnemyKilled;
+    [SerializeField] float health, maxHeath = 3f;
+
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHeath;
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target)
+        {
+            Vector2 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+
+
+        }
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        Debug.Log($"Damage Amount: {damageAmount}");
+        health -= damageAmount;
+        Debug.Log($"Health is now: {health}");
+
+        if(health<=0)
+        {
+            Destroy(gameObject);
+            OnEnemyKilled?.Invoke(this);
+        }
+    }
+
+   // private void OnCollisionEnter2D(Collision2D collision)
+   // {
+        
+ //   }
+}
